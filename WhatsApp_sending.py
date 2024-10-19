d58278a012636b00566bc8c38e4d91d691e66b0e
Merrivoir <merrivoir@gmail.com>
2024-10-19 21:07:46 +0500

    Библиотека PyWhatKit

--- WhatsApp_sending.py
+++ WhatsApp_sending.py
@@ -1,24 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By as By
+from selenium.webdriver.support.ui import WebDriverWait as WDWait
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, UnexpectedAlertPresentException
 from time import sleep
+from datetime import datetime
 import urllib.parse
 import random
 
 #блок импорта номеров и сообщений
-from Aiqerim import leads3_aq as numbers
-from Aiqerim import message_aq as message
+from Aiqerim import nm_aq as numbers
+from Aiqerim import message_aq1 as message
+from CashFlow_chain import msg_t1 as msg
+from CashFlow_chain import phoneNumbers1 as numbers
 
 #Элементы на странице
 modal_elem = '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[1]'
 modal_btn = '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[2]/div/button'
-send_btn = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
+#send_btn = '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[2]/button'
+send_btn = '/html/body/div[1]/div/div/div[2]/div[4]/div/footer/div[1]/div/span/div/div[2]/div[2]/button'
 
-phonenumber = input('Введите номер от имени которого нужна рассылка: ')
+defaultDelay = 30
+phonenumber = input("Введите номер от имени которого нужна рассылка: ")
+delay = input(f"Введите значение задержки (по умолчанию {defaultDelay}): ")
+delay = int(delay) if delay.strip() else defaultDelay
 
 options = webdriver.ChromeOptions()
 options.add_argument('--allow-profiles-outside-user-dir')
@@ -31,17 +36,19 @@ options.add_argument('--unexpectedAlertBehaviour')
 
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver = webdriver.Chrome(options=options)
-wait = WebDriverWait(driver, 30)
+wait = WDWait(driver, delay)
 
-nm_test = ["+77777777777", "+77474728450"]
-encoded_string = urllib.parse.quote(message)
+print(f"""Количество номеров: {len(numbers)}""")
+with open("Log/results.txt", "a") as file:
+    file.write(f"Старт рассылки: {datetime.now()}\n")
+encoded_string = urllib.parse.quote(msg)
 
 i = 0
 send = [] #успешно отправлено
 abort =[] #ошибка отправки
 unreg = [] #незарегистрированные номера
 
-for number in nm_test:
+for number in numbers:
     try:
         i = i + 1
         
@@ -50,20 +57,28 @@ for number in nm_test:
         
         modal = wait.until(EC.visibility_of_element_located((By.XPATH, modal_elem)))
         modal_text = modal.text
+        print(f'Текст модального окна:\n{modal_text}')
+
+        if modal_text == 'Начало чата':
+            pass
         
         if modal_text == 'Номер телефона, отправленный по ссылке, недействительный.':
             driver.find_element(By.XPATH, modal_btn).click()
             print(f"{i}. Номер {number} не зарегистрирован в WhatsApp")
+            with open("Log/results.txt", "a") as file:
+                file.write(f"{i}. Номер {number} не зарегистрирован в WhatsApp\n")
             unreg.append(number)
             sleep(random.uniform(1,2))
             continue
-
+        
         wait.until(EC.element_to_be_clickable((By.XPATH, send_btn)))
         driver.find_element(By.XPATH, send_btn).click()
         
         print(f"{i}. Сообщение на номер {number} отправлено")
+        with open("Log/results.txt", "a") as file:
+            file.write(f"{i}. Сообщение на номер {number} отправлено\n")
         send.append(number)
-        sleep(random.uniform(1,2))
+        sleep(random.uniform(2,3))
 
     except NoSuchElementException:
         print(f"Элемент для номера {number} не найден, пропуск.")
@@ -85,8 +100,10 @@ for number in nm_test:
         continue
 
 print(f'Отправлено: {len(send)}')
-print(f'Ошибки отправки: {len(abort)}')
-print(f'Незаригистрированные номера: {len(unreg)}')
+print(f'Ошибки отправки: {len(abort)}, {abort}')
+print(f'Незаригистрированные номера: {len(unreg)}, {unreg}')
+with open("Log/results.txt", "a") as file:
+    file.write(f"{abort}\n{unreg}\n")
 
 #/html/body/div[1]/div/div/span[2]/div/span/div/div/div/div/div/div[1]
 #//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[1]
