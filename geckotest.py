9c698c4905fdbe80e2999271192c9005b85e744d
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2025-03-02 21:54:28 +0500

    Update geckotest.py

--- geckotest.py
+++ geckotest.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 service = Service(log_path="geckodriver.log")
 
@@ -13,14 +11,11 @@ options.add_argument("--headless")  # Включение headless режима
 options.add_argument("--disable-gpu")  # Отключение GPU
 options.add_argument("--no-sandbox")  # Отключение sandbox (важно для контейнеров)
 options.add_argument("--disable-dev-shm-usage")  # Использование /tmp вместо /dev/shm
-
-# Настройка таймаутов
-caps = DesiredCapabilities.FIREFOX
-caps["pageLoadStrategy"] = "eager"  # Ускоряет загрузку страницы
+options.set_capability("pageLoadStrategy", "eager")  # Ускоряет загрузку страницы
 
 try:
     # Запуск браузера
-    driver = webdriver.Firefox(service=service, options=options, desired_capabilities=caps) #driver = webdriver.Firefox(service=service, options=options)
+    driver = webdriver.Firefox(service=service, options=options) #driver = webdriver.Firefox(service=service, options=options)
 
     # Тестовый запрос
     driver.get("https://www.google.com")
